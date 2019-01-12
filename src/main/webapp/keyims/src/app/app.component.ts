8a576a415c1c139230a6467900e18d43618da291
mayureshiyer <msiyer227@gmail.com>
2019-01-12 18:13:00 -0500

    updated angular

--- src/main/webapp/keyims/src/app/app.component.ts
+++ src/main/webapp/keyims/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { KeyComponent } from './key/key.component';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +6,5 @@ import { KeyComponent } from './key/key.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'KeyIMS';
+  title = 'keyims';
 }
--- src/main/webapp/keyims/src/app/app.module.ts
+++ src/main/webapp/keyims/src/app/app.module.ts
@@ -1,42 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-<<<<<<< HEAD
-import { UrlService } from './shared/url.service';
-import { UserService} from './shared/user/user.service';
-=======
-import { KeyComponent } from './key/key.component';
-import { KeyServiceService } from './key-service.service';
-import { HttpClientModule } from '@angular/common/http';
->>>>>>> 76af4c7a70a6fa20004efb14243188362109e4e7
 
 @NgModule({
   declarations: [
-    AppComponent,
-<<<<<<< HEAD
-    LoginComponent
-=======
-    LoginComponent,
-    KeyComponent
->>>>>>> 76af4c7a70a6fa20004efb14243188362109e4e7
+    AppComponent
   ],
   imports: [
-  RouterModule,
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
+    BrowserModule
   ],
-<<<<<<< HEAD
-  providers: [
-      UrlService,
-      UserService
-  ],
-=======
-  providers: [KeyServiceService],
->>>>>>> 76af4c7a70a6fa20004efb14243188362109e4e7
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
