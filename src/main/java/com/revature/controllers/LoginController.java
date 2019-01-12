68619ceda51e15548a466f34a015e5a34dda4065
Warren Chan <warrenchan31@gmail.com>
2019-01-12 15:54:21 -0500

    update

--- src/main/java/com/revature/controllers/LoginController.java
+++ src/main/java/com/revature/controllers/LoginController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.revature.beans.User;
 import com.revature.services.UserService;
@@ -17,26 +18,19 @@ public class LoginController
 	@Autowired
 	private UserService us;
 	
-	@RequestMapping(method=RequestMethod.GET)
-	public String goLogin(HttpSession sess) 
-	{
-		if (sess.getAttribute("user") != null)
-			return "redirect:home";
-		return "index.html";
-	}
-	
 	@RequestMapping(method=RequestMethod.POST)
+	@ResponseBody
 	public String login(String username, String password, HttpSession session) 
 	{
 		User u = us.login(username, password);
 		if (u == null) 
 		{
-			return "redirect:login";
+			return "Failed";
 		}
 		else
 		{
 			session.setAttribute("user", u);
-			return "redirect:home";
+			return "Welcome "+username;
 		}
 	}
 
