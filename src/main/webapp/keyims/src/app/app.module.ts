4fd3118ecf7820f5fb38379d0856aaaf1cfc765f
Warren Chan <warrenchan31@gmail.com>
2019-01-21 15:47:00 -0500

    override

--- src/main/webapp/keyims/src/app/app.module.ts
+++ src/main/webapp/keyims/src/app/app.module.ts
@@ -7,11 +7,11 @@ import { AppComponent } from './app.component';
 import { KeyComponent } from './key/key.component';
 import { LoginComponent } from './login/login.component';
 
-import { KeyServiceService } from './key-service.service';
-import { HttpClientModule } from '@angular/common/http';
 import { UrlService } from './shared/url.service';
 import { UserService} from './shared/user/user.service';
-import { AuditComponent } from './audit/audit.component';
+
+import { KeyServiceService } from './key-service.service';
+import { HttpClientModule } from '@angular/common/http';
 import { MsgComponent } from './msg/msg.component';
 
 
@@ -20,7 +20,6 @@ import { MsgComponent } from './msg/msg.component';
     AppComponent,
     LoginComponent,
     KeyComponent,
-    AuditComponent,
     MsgComponent
   ],
   imports: [
@@ -35,6 +34,7 @@ import { MsgComponent } from './msg/msg.component';
       UserService,
       KeyServiceService
   ],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
